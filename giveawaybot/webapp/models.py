4371ed4c286a780cbe951d0a7275d95b0aaccd18
Alexander Valiev <worxvaliev@gmail.com>
2024-09-26 19:25:16 +0500

    add кref system

--- giveawaybot/webapp/models.py
+++ giveawaybot/webapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.crypto import get_random_string
 
 
 class TelegramUser(models.Model):
@@ -8,9 +9,47 @@ class TelegramUser(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     username = models.CharField(max_length=128, null=True, blank=True)
+    referral_code = models.CharField(max_length=10, blank=True, null=True)
+    referrer = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='referrals')
+    referral_bonus = models.IntegerField(verbose_name="Количество бонусов от рефералов", default=0)
 
     def __str__(self):
         return self.username
+    def generate_referral_code(self):
+        return get_random_string(length=10)
+
+    def register_user(self, telegram_id, first_name, last_name, referrer_code=None):
+        user = TelegramUser.objects.create(
+            telegram_id=telegram_id,
+            first_name=first_name,
+            last_name=last_name,
+            referral_code=self.generate_referral_code()
+        )
+
+        if referrer_code:
+            try:
+                referrer = TelegramUser.objects.get(referral_code=referrer_code)
+                user.referrer = referrer
+                user.save()
+
+                # Награда для реферера
+                referrer.referral_bonus += 1
+                referrer.save()
+
+                # Записываем информацию о награде
+                ReferralReward.objects.create(user=user, referrer=referrer)
+            except TelegramUser.DoesNotExist:
+                pass  # Обработка ошибки: реферальный код не найден
+
+
+class ReferralReward(models.Model):
+    """Модель для отслеживания наград за рефералов"""
+    user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE)
+    referrer = models.ForeignKey(TelegramUser, related_name='rewards', on_delete=models.CASCADE)
+    reward_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} referred by {self.referrer}"
 
 
 class Prize(models.Model):
@@ -29,6 +68,7 @@ class GiveAway(models.Model):
     description = models.CharField(verbose_name="Описание", max_length=254)
     image = models.ImageField(verbose_name="Изображение", upload_to="images/%Y/%m/")
     create_date = models.DateField(verbose_name="Дата создания")
+    start_date = models.DateField(verbose_name="Дата начала")
     winners_count = models.IntegerField(verbose_name="Количество призовых мест")
     prize = models.ForeignKey(Prize, verbose_name="Приз", on_delete=models.CASCADE)
 
@@ -37,7 +77,7 @@ class GiveAway(models.Model):
 
 
 class Ticket(models.Model):
-    """Модель тикета пользователя"""
+    """Модель билета для розыгрыша"""
     giveaway = models.ForeignKey(GiveAway, verbose_name="Розыгрыш", on_delete=models.CASCADE)
     participant = models.ForeignKey(TelegramUser, verbose_name="Участник", on_delete=models.CASCADE)
     create_date = models.DateField(verbose_name="Дата создания")
