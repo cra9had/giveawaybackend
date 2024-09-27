dcff99cc76e38a26895b06f33e4d2abe253ca810
Alexander Valiev <worxvaliev@gmail.com>
2024-09-27 10:55:44 +0500

    add join/giveaway_id

--- giveawaybot/webapp/views.py
+++ giveawaybot/webapp/views.py
@@ -3,6 +3,7 @@ from .models import TelegramUser, GiveAway, Ticket
 from .serializers import TelegramUserSerializer, GiveAwaySerializer, TicketSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
 
 from drf_spectacular.utils import (
     # OpenApiParameter,
@@ -62,8 +63,26 @@ class TicketViewSet(viewsets.ModelViewSet):
     serializer_class = TicketSerializer
     http_method_names = ['post']
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    @action(detail=False, methods=['post'], url_path='join/(?P<giveaway_id>[^/.]+)')
+    def join_giveaway(self, request, giveaway_id=None):
+        """Метод выдачи билета участнику"""
+        try:
+            giveaway = GiveAway.objects.get(id=giveaway_id)
+            participant_id = request.data.get("participant_id")
+            participant = TelegramUser.objects.get(id=participant_id)
+
+            # Проверяем, есть ли уже билет у участника для данного розыгрыша
+            existing_ticket = Ticket.objects.filter(giveaway=giveaway, participant=participant).first()
+            if existing_ticket:
+                return Response({"error": "Participant has already joined this giveaway."},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            # Создаем новый билет
+            ticket = Ticket.create_ticket(giveaway=giveaway, participant=participant)
+            return Response({"ticket_number": ticket.number_ticket}, status=status.HTTP_201_CREATED)
+        except GiveAway.DoesNotExist:
+            return Response({"error": "Giveaway not found."}, status=status.HTTP_404_NOT_FOUND)
+        except TelegramUser.DoesNotExist:
+            return Response({"error": "Participant not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        # TODO: Дорабоать
