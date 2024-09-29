0bb0e3d33cfb0f8e22384d42643b52abc2e3f257
khaiam__Mac <khaiam.aliev@mail.ru>
2024-09-29 15:59:35 +0700

    Models refactoring

--- giveawaybot/webapp/models.py
+++ giveawaybot/webapp/models.py
@@ -5,24 +5,24 @@ class TelegramUser(models.Model):
     """Модель пользователя"""
     telegram_id = models.CharField(max_length=128, unique=True)
     is_bot = models.BooleanField(default=False)
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=128, null=True, blank=True)
+    last_name = models.CharField(max_length=128, null=True, blank=True)
     username = models.CharField(max_length=128, null=True, blank=True)
 
     def __str__(self):
-        return self.username or self.telegram_id
+        return f"@{self.username}" if self.username else str(self.telegram_id)
 
 
 class GiveAway(models.Model):
     """Модель розыгрыша"""
-    title = models.CharField(verbose_name="Название", max_length=100)
-    description = models.CharField(verbose_name="Описание", max_length=254)
+    title = models.CharField(verbose_name="Название", max_length=50)
+    description = models.CharField(verbose_name="Описание", max_length=2000)
     image = models.ImageField(verbose_name="Изображение", upload_to="images/%Y/%m/", blank=True, null=True)
-    create_date = models.DateField(verbose_name="Дата создания")
+    create_date = models.DateField(verbose_name="Дата создания", auto_now_add=True)
     end_datetime = models.DateTimeField(verbose_name="Время завершения розыгрыша")
     winners_count = models.IntegerField(verbose_name="Количество призовых мест")
     is_referral_system = models.BooleanField(verbose_name="Реферальная система", default=False)
-    referral_invites_count = models.IntegerField(verbose_name="Количество приглашений", null=True, blank=True)
+    referral_invites_count = models.IntegerField(verbose_name="Количество приглашений для нового билета", null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -32,7 +32,7 @@ class Ticket(models.Model):
     """Модель билета для розыгрыша"""
     giveaway = models.ForeignKey(GiveAway, verbose_name="Розыгрыш", on_delete=models.CASCADE)
     participant = models.ForeignKey(TelegramUser, verbose_name="Участник", on_delete=models.CASCADE)
-    create_date = models.DateField(verbose_name="Дата создания")
+    create_date = models.DateTimeField(verbose_name="Время создания", auto_now_add=True)
     number_ticket = models.IntegerField(verbose_name="Номер тикета")
 
     def __str__(self):
